683ecd2981a9223e9b1dcccc3a6e933e2135c196
Ender Pina <enderjpinar.dev@gmail.com>
2025-10-08 10:07:30 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "your-api-key",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  "projectId": "studio-7601782447-44d81",
+  "appId": "1:775892034675:web:98ed2724bcaff2ed427606",
+  "apiKey": "AIzaSyCnXU8XU7ZzA_12CDaYaY9W2rWBmkGLB-g",
+  "authDomain": "studio-7601782447-44d81.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "775892034675"
 };
 
 // Initialize Firebase
