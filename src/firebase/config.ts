0b4bd95e762cd7f4dc26f763349524a40ea354aa
Ender Pina <enderjpinar.dev@gmail.com>
2025-10-08 10:09:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7601782447-44d81",
+  "appId": "1:775892034675:web:98ed2724bcaff2ed427606",
+  "apiKey": "AIzaSyCnXU8XU7ZzA_12CDaYaY9W2rWBmkGLB-g",
+  "authDomain": "studio-7601782447-44d81.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "775892034675"
+};
